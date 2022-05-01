ee6d23fd30e1890ea440252610f747f75c720e1c
jake <rencelaw70@gmail.com>
2022-05-01 09:26:13 +0800

    Added clickable cards

--- src/app/main/home-screen/crops-screen/crops-screen.page.ts
+++ src/app/main/home-screen/crops-screen/crops-screen.page.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crops-screen.page.scss'],
 })
 export class CropsScreenPage implements OnInit {
+  isSelectedWaterMelon = false;
+  isSelectedBanana = false;
+  isSelectedMelon = false;
+  isSelectedRice = false;
+  isSelectedEggPlant = false;
+  isSelectedBeans = false;
+  isSelectedPig = false;
+  isSelectedChicken = false;
+  isSelectedDuck = false;
 
-  constructor() { }
+  constructor() {}
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClickWaterMelon() {
+    this.isSelectedWaterMelon = !this.isSelectedWaterMelon;
   }
-
+  onClickBanana() {
+    this.isSelectedBanana = !this.isSelectedBanana;
+  }
+  onClickMelon() {
+    this.isSelectedMelon = !this.isSelectedMelon;
+  }
+  onClickRice() {
+    this.isSelectedRice = !this.isSelectedRice;
+  }
+  onClickEggPlant() {
+    this.isSelectedEggPlant = !this.isSelectedEggPlant;
+  }
+  onClickBeans() {
+    this.isSelectedBeans = !this.isSelectedBeans;
+  }
+  onClickPig() {
+    this.isSelectedPig = !this.isSelectedPig;
+  }
+  onClickChicken() {
+    this.isSelectedChicken = !this.isSelectedChicken;
+  }
+  onClickDuck() {
+    this.isSelectedDuck = !this.isSelectedDuck;
+  }
 }
