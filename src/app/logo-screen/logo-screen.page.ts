d803307de49d06a98d0cbd6073093d0a958c5991
jake <rencelaw70@gmail.com>
2022-09-25 09:15:18 +0800

    designing the profile page

--- src/app/logo-screen/logo-screen.page.ts
+++ src/app/logo-screen/logo-screen.page.ts
@@ -29,9 +29,9 @@ export class LogoScreenPage implements OnInit {
     // this.router.navigate(['/profile-setup/crops-or-livestock/select-crops']);
     // this.router.navigate(['/main/tabs/community/edit']);
 
-    // this.router.navigate(['/main/tabs/farming-method']);
+    this.router.navigate(['/main/tabs/profile']);
 
-    this.router.navigate(['/onboarding/screen1']);
+    // this.router.navigate(['/onboarding/screen1']);
     // this.router.navigate(['/auth-page/sign-up']);
     // this.router.navigate(['/main/tabs/farming-method']);
   }
