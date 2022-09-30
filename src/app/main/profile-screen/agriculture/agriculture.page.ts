7ad5792ca1ee4d19a8475dd9dad9b9b38d644dbc
jake <rencelaw70@gmail.com>
2022-09-30 19:39:18 +0800

    fixed home page hide the title of doesn't  selected agri

--- src/app/main/profile-screen/agriculture/agriculture.page.ts
+++ src/app/main/profile-screen/agriculture/agriculture.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeCropsService } from '../../home-screen/home-crops.service';
 import { HomeLivestockService } from '../../home-screen/home-livestock.service';
+import { MainService } from '../../main.service';
 @Component({
   selector: 'app-agriculture',
   templateUrl: './agriculture.page.html',
@@ -12,15 +13,25 @@ export class AgriculturePage implements OnInit {
   availableAgri = [];
   constructor(
     private homeCropsService: HomeCropsService,
-    private homeLivestockService: HomeLivestockService
+    private homeLivestockService: HomeLivestockService,
+    private mainService: MainService
   ) {}
 
   ngOnInit() {
+    //get selected crops
     this.userCurrentSelectedCrops = this.homeCropsService.getCropsData();
+    //get selected crops
     this.userCurrentSelectedLivestock =
       this.homeLivestockService.getLivestockData();
-
     console.log(this.userCurrentSelectedCrops);
-    console.log(this.userCurrentSelectedLivestock);
+
+    //get all livestock
+    this.mainService
+      .getAllCropsData()
+      ?.map((item) => this.availableAgri.push(item));
+    //get all livestock
+    this.mainService
+      .getAllLivestockData()
+      ?.map((item) => this.availableAgri.push(item));
   }
 }
